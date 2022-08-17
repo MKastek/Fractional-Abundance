bb1af7522b79d5c61d8ab39c54d6546b91a21ff0
MKastek <mkastek8@gmail.com>
2022-08-17 10:03:15 +0200

    added numba concurency

--- FractionalAbundance.py
+++ FractionalAbundance.py
@@ -17,7 +17,7 @@ plt.rcParams['figure.figsize'] = [12, 7]
 
 class FractionalAbundance:
 
-    def __init__(self, atom, concurrent = False, numba=False):
+    def __init__(self, atom, concurrent = False):
 
         self.atom = atom
         self.ACD_file, self.SCD_file= self.select_files()
@@ -36,22 +36,17 @@ class FractionalAbundance:
 
         self.ACD_matrix, self.SCD_matrix = self.read_coefficients_matrices(self.ACD_file,type='ACD'), self.read_coefficients_matrices(self.SCD_file, type='SCD')
 
-        if numba:
-            self.SCD_matrix_dict = Dict()
-            self.ACD_matrix_dict = Dict()
-            for k, v in self.SCD_matrix.items():
-                self.SCD_matrix_dict[k] = v
-            for k, v in self.ACD_matrix.items():
-                self.ACD_matrix_dict[k] = v
+
+        self.SCD_matrix_dict = Dict()
+        self.ACD_matrix_dict = Dict()
+        for k, v in self.SCD_matrix.items():
+            self.SCD_matrix_dict[k] = v
+        for k, v in self.ACD_matrix.items():
+            self.ACD_matrix_dict[k] = v
 
         if not concurrent:
-            self.FA_arr = []
+            self.FA_arr = [self.get_Fractional_Abundance(SCD_matrix=self.SCD_matrix_dict,ACD_matrix=self.ACD_matrix_dict,ion=ion,Z=self.Z) for ion in range(self.Z +1)]
 
-            for ion in range(self.Z + 1):
-                if numba:
-                    self.FA_arr.append(self.get_Fractional_Abundance_numba(SCD_matrix=self.SCD_matrix_dict,ACD_matrix=self.ACD_matrix_dict,ion=ion,Z=self.Z))
-                else:
-                    self.FA_arr.append(self.get_Fractional_Abundance(ion=ion))
 
     def select_files(self):
         filenames = os.listdir(os.path.join('data','unresolved'))
@@ -118,7 +113,7 @@ class FractionalAbundance:
 
     @staticmethod
     @numba.njit(parallel=True)
-    def get_Fractional_Abundance_numba(SCD_matrix,ACD_matrix, ion,Z):
+    def get_Fractional_Abundance(SCD_matrix,ACD_matrix, ion,Z):
         K = [np.divide(10 ** SCD_matrix[str(i) + str(i + 1)], 10 ** ACD_matrix[str(i + 1) + str(i)]) for i in range(Z)]
 
         K.insert(0, np.ones_like(K[0]))
@@ -134,15 +129,6 @@ class FractionalAbundance:
         #product_all = np.cumprod(K,axis=0)
         #FA = np.divide(product_all[ion], np.sum(product_all,axis=0))
         FA = np.divide(product_all[ion], sum_all)
-        return FA
-
-    def get_Fractional_Abundance(self, ion):
-        K = [np.divide(10 ** self.SCD_matrix[str(i) + str(i + 1)], 10 ** self.ACD_matrix[str(i + 1) + str(i)]) for i in
-             range(self.Z)]
-        K.insert(0, np.ones_like(K[0]))
-        product_all = np.cumprod(K, axis=0)
-
-        FA = np.divide(product_all[ion], np.sum(product_all, axis=0))
         return FA
 
     def plot_FA_all(self, i_Ne=50):
@@ -161,14 +147,14 @@ class FractionalAbundance:
 
         plt.show()
 
-    def worker(self,ion):
-        fun = self.get_Fractional_Abundance(ion=ion)
+    def worker(self,SCD_matrix,ACD_matrix,ion,Z):
+        fun = self.get_Fractional_Abundance(SCD_matrix=SCD_matrix,ACD_matrix=ACD_matrix,ion=ion,Z=Z)
         return fun
 
     def calculate(self):
         ion_list = list(range(self.Z + 1))
         pool = ThreadPoolExecutor(self.Z + 1)
-        pp = [pool.submit(self.worker,ion=ion) for ion in range(len(ion_list))]
+        pp = [pool.submit(self.worker,SCD_matrix=self.SCD_matrix_dict,ACD_matrix=self.ACD_matrix_dict,ion=ion,Z=self.Z) for ion in range(len(ion_list))]
         for p in pp:
             self.FA_arr.append(p.result())
 
@@ -185,12 +171,5 @@ class FractionalAbundance:
 
 #t1 = time.time()
 if __name__ == '__main__':
-    print('Numba')
-    FA = FractionalAbundance(atom='Mo', numba=True)
-    FA.create_dataset(filename='numba')
-    FA.plot_FA_all()
-    print('No Numba')
-    FA = FractionalAbundance(atom='Mo', numba=False)
-    FA.create_dataset(filename='nonumba')
-    FA.plot_FA_all()
-
+    FA = FractionalAbundance(atom='Mo',concurrent=True)
+    FA.calculate()
